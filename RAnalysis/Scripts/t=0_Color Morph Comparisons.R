ba256b20063d182c7d2b95c55ebe9d30cebcd314
dconetta <48772113+dconetta@users.noreply.github.com>
2019-07-18 17:45:46 -0400

    Update t=0_Color Morph Comparisons.R

--- RAnalysis/Scripts/t=0_Color Morph Comparisons.R
+++ RAnalysis/Scripts/t=0_Color Morph Comparisons.R
@@ -24,21 +24,67 @@ boxplot(Red.Norm.Coral~Colormorph, data = t0, main = "Color Morph Red Channel Co
 boxplot(Green.Norm.Coral~Colormorph, data = t0, main = "Color Morph Green Channel Comparisons", xlab = "Color Morph", ylab = "Normalized Red values", col = c("orange", "brown"))
 boxplot(Blue.Norm.Coral~Colormorph, data = t0, main = "Color Morph Blue Channel Comparisons", xlab = "Color Morph", ylab = "Normalized Red values", col = c("orange", "brown"))
 
-t.test(Red.Norm.Coral~Colormorph, data = t0) #significantly different
-t.test(Green.Norm.Coral~Colormorph, data = t0) #not significantly different
-t.test(Blue.Norm.Coral~Colormorph, data = t0) #not significantly different
+> t.test(Red.Norm.Coral~Colormorph, data = t0)
 
+	Welch Two Sample t-test
 
+data:  Red.Norm.Coral by Colormorph
+t = 2.297, df = 36.885, p-value = 0.0274
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ 0.006285161 0.100430086
+sample estimates:
+mean in group Orange    mean in group Red 
+           0.5878015            0.5344439 
+
+> t.test(Green.Norm.Coral~Colormorph, data = t0)
+
+	Welch Two Sample t-test
+
+data:  Green.Norm.Coral by Colormorph
+t = 1.5954, df = 39.842, p-value = 0.1185
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -0.01072012  0.09102036
+sample estimates:
+mean in group Orange    mean in group Red 
+           0.3834943            0.3433442 
+
+> t.test(Blue.Norm.Coral~Colormorph, data = t0)
+
+	Welch Two Sample t-test
+
+data:  Blue.Norm.Coral by Colormorph
+t = -1.9227, df = 47.182, p-value = 0.06056
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -0.167359789  0.003776948
+sample estimates:
+mean in group Orange    mean in group Red 
+           0.2177272            0.2995187 
+___________________________________________________________________________________________________________________________
 #ZOOX DENSITY COMPARISON
 setwd("C:/Users/dcone/OneDrive/Documents/Northeastern/Hawaii/Nursery Coloration Experiment/Zoox Data") #set working
 Data1 <- read.csv("t=0_data.csv", header=T, sep=",", na.string="NA") #read in data file
 
 
 boxplot(sym_density~colormorph, data = Data1, main = "Color Morph Zoox Density Comparisons at T=0", xlab = "Color Morph", ylab = "Zoox Density (cells/cm2)", ylim=c(0, 1.5e+07), col = c("orange", "brown"))
-t.test(sym_density~colormorph, data = Data1) #significantly different between colormorphs
+> t.test(sym_density~colormorph, data = Data1) #significantly different between colormorphs
+
+	Welch Two Sample t-test
+
+data:  sym_density by colormorph
+t = 2.2268, df = 43.747, p-value = 0.03116
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+  177418.8 3566236.7
+sample estimates:
+mean in group orange    mean in group red 
+             6531727              4659900 
 
 #Took out major outliers which were B1_144, B1_23, B2_170, O9_95, O13_202 and O13_196 (they were way above the rest and still have n=46)
 
+_______________________________________________________________________________________________________________________________
 #PROTEIN DENSITY COMPARISON
 setwd("C:/Users/dcone/OneDrive/Documents/Northeastern/Hawaii/Nursery Coloration Experiment/Protein Assay Data/2019 Data") #set working
 Data2 <- read.csv("Protein_Data.csv", header=T, sep=",", na.string="NA") #read in data file
@@ -46,8 +92,20 @@ t_0=subset(Data2, timepoint == "06/09/17" & protein_density < 15000) #Excluding 
 
 
 boxplot(protein_density~colormorph, data = t_0, main = "Color Morph Protein Density Comparisons at T=0", xlab = "Color Morph", ylab = "Protein Density at T=0 (ug/cm2)", col = c("orange", "brown"))
-t.test(protein_density~colormorph, data = t_0) #not significantly different 
+> t.test(protein_density~colormorph, data = t_0) #not significantly different
 
+	Welch Two Sample t-test
+
+data:  protein_density by colormorph
+t = 0.24158, df = 43.982, p-value = 0.8102
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -1078.639  1372.442
+sample estimates:
+mean in group orange    mean in group red 
+            3947.324             3800.423 
+
+__________________________________________________________________________________________________________________________________
 #ALL PLOTS TOGETHER
 par(mfrow=c(1,3))
 boxplot(Red.Norm.Coral~Colormorph, data = t0, main = "Color Morph Red Channel Comparisons at T=0", xlab = "Color Morph", ylab = "Normalized Red values", col = c("orange", "brown"))
