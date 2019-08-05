4a82ec9ead789c7355f929113f11fc0be193cf01
Hollie Putnam <hollieputnam@gmail.com>
2019-08-05 12:19:56 -0400

    updated stats

--- RAnalysis/Scripts/Mixed Models with Repeated Measures.R
+++ RAnalysis/Scripts/Mixed Models with Repeated Measures.R
@@ -6,32 +6,33 @@ library("nlme")
 rm(list=ls()) #clears workspace 
 
 #Set Working Directory
-setwd("C:/Users/dcone/OneDrive/Documents/Northeastern/Hawaii/Nursery Coloration Experiment/Zoox Data")#set working directory
+#setwd("C:/Users/dcone/OneDrive/Documents/Northeastern/Hawaii/Nursery Coloration Experiment/Zoox Data")#set working directory
+setwd("~/MyProjects/Montipora-Colormorphs-Project/RAnalysis/Data") #set working directory
 
 #_________________________________#
 #Trying with Zoox Data First
-Data1 <- read.csv("Zoox_Count_Data_Shade.v.Shallow.csv") #Data for only Shade & Shallow treatment 
+Data1 <- read.csv("Metadata.csv") #Data for only Shade & Shallow treatment 
 Data1[Data1 == 0] <- NA #All zeroes in data will just be referred to as NA
 
 #Mixed Effects Three Way Repeated Measures ANOVA
 
-All.Means1 <- ddply(Data1, c("timepoint", "frag.id", "Treatment", "Colormorph"), summarize,
-                    mean= mean(sym.density, na.rm=T), #mean pnet
-                    N = sum(!is.na(sym.density)), # sample size
-                    se = sd(sym.density, na.rm=T)/sqrt(N)) #SE
-All.Means1
-All.Means1$se[is.na(All.Means1$se)] <- 0
-All.Means1$Group <- paste(All.Means1$timepoint, All.Means1$Treatment, All.Means1$Colormorph)
-All.Means1$SpGroup <- paste(All.Means1$Treatment, All.Means1$Colormorph)
+# All.Means1 <- ddply(Data1, c("timepoint", "frag.id", "Treatment", "Colormorph"), summarize,
+#                     mean= mean(sym.density, na.rm=T), #mean pnet
+#                     N = sum(!is.na(sym.density)), # sample size
+#                     se = sd(sym.density, na.rm=T)/sqrt(N)) #SE
+# All.Means1
+# All.Means1$se[is.na(All.Means1$se)] <- 0
+# All.Means1$Group <- paste(All.Means1$timepoint, All.Means1$Treatment, All.Means1$Colormorph)
+# All.Means1$SpGroup <- paste(All.Means1$Treatment, All.Means1$Colormorph)
 
-attach(All.Means1)
-Time = factor(timepoint)
-Group = factor(Colormorph)
-Group2 = factor(Treatment)
-Subject = factor(frag.id)
-library(lme4)
+# attach(All.Means1)
+# Time = factor(timepoint)
+# Group = factor(Colormorph)
+# Group2 = factor(Treatment)
+# Subject = factor(frag.id)
 
-model2 <- lmer(mean~timepoint + Group + Group2 + Time:Group:Group2 + (1|Subject), data = All.Means1)
+
+model2 <- lm(protein.density~ Treatment + Colormorph+timepoint + timepoint:Treatment:Colormorph, data = Data1)
 summary(model2)
 anova(model2)
 fixef(model2)
