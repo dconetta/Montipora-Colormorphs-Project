f95e8dbc12c0110b2c323f52dd2b3aee5ea577f9
dconetta <48772113+dconetta@users.noreply.github.com>
2019-08-05 09:48:30 -0400

    Add files via upload

---
+++ RAnalysis/Scripts/Mixed Models with Repeated Measures.R
@@ -0,0 +1,45 @@
+#Mix Models with Repeated Measures ANOVA
+library("lme4")
+library("carData")
+library("nlme")
+
+rm(list=ls()) #clears workspace 
+
+#Set Working Directory
+setwd("C:/Users/dcone/OneDrive/Documents/Northeastern/Hawaii/Nursery Coloration Experiment/Zoox Data")#set working directory
+
+#_________________________________#
+#Trying with Zoox Data First
+Data1 <- read.csv("Zoox_Count_Data_Shade.v.Shallow.csv") #Data for only Shade & Shallow treatment 
+Data1[Data1 == 0] <- NA #All zeroes in data will just be referred to as NA
+
+#Mixed Effects Three Way Repeated Measures ANOVA
+
+All.Means1 <- ddply(Data1, c("timepoint", "frag.id", "Treatment", "Colormorph"), summarize,
+                    mean= mean(sym.density, na.rm=T), #mean pnet
+                    N = sum(!is.na(sym.density)), # sample size
+                    se = sd(sym.density, na.rm=T)/sqrt(N)) #SE
+All.Means1
+All.Means1$se[is.na(All.Means1$se)] <- 0
+All.Means1$Group <- paste(All.Means1$timepoint, All.Means1$Treatment, All.Means1$Colormorph)
+All.Means1$SpGroup <- paste(All.Means1$Treatment, All.Means1$Colormorph)
+
+attach(All.Means1)
+Time = factor(timepoint)
+Group = factor(Colormorph)
+Group2 = factor(Treatment)
+Subject = factor(frag.id)
+library(lme4)
+
+model2 <- lmer(mean~timepoint + Group + Group2 + Time:Group:Group2 + (1|Subject), data = All.Means1)
+summary(model2)
+anova(model2)
+fixef(model2)
+ranef(model2)
+
+# Now use the following model which allows for groups to have different slopes over time.
+# Compare this with the SAS model with an unstructured correlation matrix.
+model3 <- lmer(dv~Time + Group + Time:Group + (Group|Subject))
+## For now ignore the error message
+summary(model3)
+anova(model3)
